dd63e2e6e3f62ce555da9c246e7f3b8b7501b751
Uroboro5 <owen_ups@hotmail.com>
2021-02-03 14:13:36 -0600

    cascaron pendientes

--- src/app/pages/pendientes/pendientes.component.ts
+++ src/app/pages/pendientes/pendientes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Usuario } from 'src/app/models/usuario';
+import { FirestoreService } from 'src/app/services/firestore.service';
 
 
 
@@ -10,13 +12,17 @@ import { Component, OnInit } from '@angular/core';
 export class PendientesComponent implements OnInit {
 
   public cargando: boolean = true;
-  
-  constructor() {
+  pendientes: Usuario[] = []; 
+  constructor(public firestoreService: FirestoreService) {
    }
  
 
   ngOnInit(): void {
     this.cargando = true;
+    this.firestoreService.getUsers().subscribe((resp)=>{
+      this.pendientes = resp.map((doc)=>new Usuario(doc.payload.doc.id,doc.payload.doc.data().celular,doc.payload.doc.data().correo,doc.payload.doc.data().estado,doc.payload.doc.data().nombre,doc.payload.doc.data().telefono,doc.payload.doc.data().tipoUsuario))
+      this.cargando = false;
+    })
 
   }
 
